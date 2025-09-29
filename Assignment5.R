fd163c75ce40db3a3e6de74b5bc9b9588d62157d
laurenlbates <156625620+laurenlbates@users.noreply.github.com>
2025-09-28 23:33:41 -0400

    Assignment5.R

---
+++ Assignment5.R
@@ -0,0 +1,15 @@
+A <- matrix(1:100,  nrow = 10)
+B <- matrix(1:1000, nrow = 10)
+
+dim(A)  # should be 10 × 10
+dim(B)  # 10 × 100 — not square
+
+#A is square, B is not square
+
+# For A
+invA <- solve(A)
+detA <- det(A)
+
+# For B, use tryCatch to capture errors
+invB <- tryCatch(solve(B), error = function(e) e)
+detB <- tryCatch(det(B),   error = function(e) e)
