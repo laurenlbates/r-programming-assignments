e288df9ef315afc132ffab67f71add483bd7877b
laurenlbates <156625620+laurenlbates@users.noreply.github.com>
2025-11-09 22:48:29 -0500

    module11_debug.R

---
+++ module11_debug.R
@@ -0,0 +1,16 @@
+set.seed(123)
+test_mat <- matrix(rnorm(50), nrow = 10)
+
+corrected_tukey <- function(x) {
+  outliers <- array(TRUE, dim = dim(x))
+  for (j in seq_len(ncol(x))) {
+    outliers[, j] <- outliers[, j] & tukey.outlier(x[, j])
+  }
+  outlier.vec <- logical(nrow(x))
+  for (i in seq_len(nrow(x))) {
+    outlier.vec[i] <- all(outliers[i, ])
+  }
+  outlier.vec
+}
+
+corrected_tukey(test_mat)
